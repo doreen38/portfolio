5719b589ef4583544540b99565e5d95a9e1e7a6e
doreen38 <88837272+doreen38@users.noreply.github.com>
2021-08-16 14:21:05 +0100

    Add files via upload

---
+++ web jounery/new figma/appl.js
@@ -0,0 +1,29 @@
+// String,Numbers, Boolean,null,undefined
+
+// Arrays
+//const fruits = ['apple','oranges','pears']
+//fruits[3]= 'grapes';
+//fruits.push('mangos');
+//fruits.unshift('strawberries');
+//console.log(Array.isArray('hello'));
+//console.log(fruits.indexOf('orange'));
+//console.log(fruits);
+const person ={
+    firstName:'John',
+    lastName: 'Doe',
+    age:30,
+    hobbies:['music','movise','sport'],
+    address:{
+        street:'50 main st',
+        city:'Bostom',
+        state:'MA'
+    }
+
+}
+console.log(person.lastName);
+//for
+ for(let i = 0; i <= 10; i++) {
+     console.log(i);
+
+ }
+
